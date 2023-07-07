225e88807d3b956e5162d932bcedccf89a17f60f
Darshan <priyadarshanmanoharan@gmail.com>
2023-07-06 21:08:44 -0700

    Final-commit

--- src/Components/FirstSection.jsx
+++ src/Components/FirstSection.jsx
@@ -1,7 +1,7 @@
 import Container from 'react-bootstrap/Container';
 import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
-import './App.css'
+import '../../src/App.css'
 import React from 'react'; import { useState } from 'react';
 import '../App.css';
 import settingImage from '.././Assets/images/setting.png';
