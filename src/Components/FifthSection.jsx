1c0ad5ca60aeb8e4f7a78b781257c29aec497087
Mysteryeditor <sri027.cvb@gmail.com>
2023-07-10 10:35:08 +0530

    a

--- src/Components/FifthSection.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-const FifthSection = () => {
-  return (
-    <div>FifthSection</div>
-  )
-}
-
-export default FifthSection
--- src/Components/FourthSection.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-const FourthSection = () => {
-  return (
-    <div>FourthSection</div>
-  )
-}
-
-export default FourthSection
